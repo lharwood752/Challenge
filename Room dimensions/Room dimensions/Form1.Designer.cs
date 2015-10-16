d6ae7cb71298dfb9e904e3aaffb1f10420490ee2
lharwood752 <lharwood752@gmail.com>
2015-10-16 14:12:26 +0100

    Program

---
+++ Room dimensions/Room dimensions/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Room_dimensions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lstResults = new System.Windows.Forms.ListBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(14, 27);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(86, 22);
+            this.txtWidth.TabIndex = 0;
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(14, 84);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(86, 22);
+            this.txtLength.TabIndex = 1;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(14, 138);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(86, 22);
+            this.txtHeight.TabIndex = 2;
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Location = new System.Drawing.Point(11, 7);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(89, 17);
+            this.lblWidth.TabIndex = 3;
+            this.lblWidth.Text = "Room Width ";
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Location = new System.Drawing.Point(11, 64);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(93, 17);
+            this.lblLength.TabIndex = 4;
+            this.lblLength.Text = "Room Length";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(11, 118);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(90, 17);
+            this.lblHeight.TabIndex = 5;
+            this.lblHeight.Text = "Room Height";
+            // 
+            // lstResults
+            // 
+            this.lstResults.FormattingEnabled = true;
+            this.lstResults.ItemHeight = 16;
+            this.lstResults.Location = new System.Drawing.Point(131, 12);
+            this.lstResults.Name = "lstResults";
+            this.lstResults.Size = new System.Drawing.Size(229, 148);
+            this.lstResults.TabIndex = 6;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(14, 179);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 35);
+            this.btnCalculate.TabIndex = 9;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(120, 179);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 35);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "m";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "m";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(99, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "m";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 225);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lstResults);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblLength);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.txtWidth);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.ListBox lstResults;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Room dimensions/Room dimensions/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Room_dimensions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string Width_Input;
+        static string Length_Input;
+        static string Height_Input;
+        static decimal Room_Width;
+        static decimal Room_Length;
+        static decimal Room_Height;
+        static decimal FloorArea;
+        static decimal WallAreas;
+        static decimal RoomVolume;
+        static decimal Paint_Required;
+        static decimal Paint_Coveravge = 12;
+
+        //Paint coverage based on 1L emulsion pot covering 12m squared.
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Calculations()
+        {
+            FloorArea = Room_Width * Room_Length;
+
+            WallAreas = ((Room_Width * 2) + (Room_Length * 2)) * Room_Height;
+
+            //Uses Width and length as room may not be a cube.
+
+            Paint_Required = WallAreas / Paint_Coveravge;
+
+            RoomVolume = Room_Width * Room_Length * Room_Height;
+        }
+
+        public void Clear()
+        {
+            lstResults.Items.Clear();
+            txtWidth.Clear();
+            txtLength.Clear();
+            txtHeight.Clear();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Width_Input = txtWidth.Text;
+                Length_Input = txtLength.Text;
+                Height_Input = txtHeight.Text;
+
+                Room_Width = Convert.ToDecimal(Width_Input);
+                Room_Length = Convert.ToDecimal(Length_Input);
+                Room_Height = Convert.ToDecimal(Height_Input);
+
+                lstResults.Items.Clear();
+
+                Calculations();
+
+                lstResults.Items.Add("Floor Area: " + decimal.Round(FloorArea, 2, MidpointRounding.AwayFromZero) + " Meters Squared");
+                lstResults.Items.Add("Paint Required: " + decimal.Round(Paint_Required, 2, MidpointRounding.AwayFromZero) + " Litres");
+                lstResults.Items.Add("Room Volume: " + decimal.Round(RoomVolume, 2, MidpointRounding.AwayFromZero) + " Meters Cubed");
+
+            }
+            catch (Exception)  //Stops crashing if invalid characters are entered.
+            {
+                MessageBox.Show("Please enter a number into all fields");
+                Clear();
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }    
+    }
+}
